539b26a76432bae6a972e8d19dfab50982c46c3b
ziyang <ziyang0621@gmail.com>
2015-07-21 10:41:15 -0700

    first commit

--- GMSPanoramaViewSwift/AppDelegate.swift
+++ GMSPanoramaViewSwift/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAEmcult29ulZu5eiEg7_0FKUYmToOHmAk");
         return true
     }
 
--- GMSPanoramaViewSwift/ViewController.swift
+++ GMSPanoramaViewSwift/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var viewStreet: UIView!
+    @IBOutlet weak var sampleImageView: UIImageView!
+    var panoView: GMSPanoramaView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let panoramaNear = CLLocationCoordinate2DMake(30.274262, -97.752371)
+        
+        panoView = GMSPanoramaView.panoramaWithFrame(CGRectZero,
+            nearCoordinate:panoramaNear)
+        
+        viewStreet.addSubview(panoView)
+        viewStreet.sendSubviewToBack(panoView)
+        
+        let equalWidth = NSLayoutConstraint(item: panoView, attribute: .Width, relatedBy: .Equal, toItem: viewStreet, attribute: .Width, multiplier: 1.0, constant: 0)
+        let equalHeight = NSLayoutConstraint(item: panoView, attribute: .Height, relatedBy: .Equal, toItem: viewStreet, attribute: .Height, multiplier: 1.0, constant: 0)
+        let top = NSLayoutConstraint(item: panoView, attribute: .Top, relatedBy: .Equal, toItem: viewStreet, attribute: .Top, multiplier: 1.0, constant: 0)
+        let left = NSLayoutConstraint(item: panoView, attribute: .Left, relatedBy: .Equal, toItem: viewStreet, attribute: .Left, multiplier: 1.0, constant: 0)
+        
+        panoView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        NSLayoutConstraint.activateConstraints([equalWidth, equalHeight, top, left])
     }
 
     override func didReceiveMemoryWarning() {
